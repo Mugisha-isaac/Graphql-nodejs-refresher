3fea409c586d2f5d9ac148e07b2808c59a098ff7
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-11-17 09:07:14 +0200

    added route to graphql to render graphiql ui and when passed status it renders status object from schema/schema

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const schema = require('./schema/schema')
+const { graphqlHTTP } = require('express-graphql')
 const PORT = process.env.port || 5000
 const app = express()
 
@@ -8,6 +9,11 @@ app.get('/', (_, res) => {
     res.send('Welcome to graphql and nodejs tutorial')
 })
 
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
 
 app.listen(PORT, () => {
     console.log(`app is running on port ${PORT}`)
