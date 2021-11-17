0cf43c9d170ac7c8ac8090e72fd5a63a954c0542
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-11-17 09:57:24 +0200

    added array of articles and contributors, defined their types and then added them in root query as fields

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,54 @@
 const graphql = require('graphql')
-const { GraphQLObjectType, GraphQLString, GraphQLId, GraphQLList, GraphQLSchema } = graphql
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList, GraphQLSchema } = graphql
+const _ = require('lodash')
 
+
+let articles = [
+    { name: 'The history of Node.js', topic: 'Node.js', date: Date.now(), id: '1', contributorId: '1' },
+    { name: 'Understanding Docker concepts', topic: 'Containers', date: Date.now(), id: '2', contributorId: '2' },
+    { name: 'Linting in Node.js using ESLint', topic: 'Node.js', date: Date.now(), id: '3', contributorId: '2' },
+    { name: 'REST APIS - Introductory quide ', topic: 'API', date: Date.now(), id: '4', contributorId: '1' }
+]
+
+
+let contributors = [
+    { name: 'John Doe', url: '/john-doe', major: 'Computer Science', id: '1' },
+    { name: 'Jane Doe', url: '/jane-doe', major: 'Physics', id: '2' }
+]
+
+const ArticleType = new GraphQLObjectType({
+    name: 'Article',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        topic: { type: GraphQLString },
+        date: { type: GraphQLString },
+        contributorId: { type: GraphQLID },
+        contributor: {
+            type: ContributorType,
+            resolve (parent, args) {
+                return _.find(contributors, { id: parent.contributorId })
+            }
+        }
+
+    })
+})
+
+const ContributorType = new GraphQLObjectType({
+    name: 'Contributor',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        url: { type: GraphQLString },
+        major: { type: GraphQLString },
+        articles: {
+            type: new GraphQLList(ArticleType),
+            resolve (parent, args) {
+                return _.filter(articles, { id: parent.id })
+            }
+        }
+    })
+})
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
@@ -9,6 +57,20 @@ const RootQuery = new GraphQLObjectType({
             type: GraphQLString,
             resolve (parent, args) {
                 return 'Welcome to graphql. Get ready to join graphql community'
+            }
+        },
+        article: {
+            type: ArticleType,
+            args: { id: { type: GraphQLID } },
+            resolve (parent, args) {
+                return _.find(articles, { 'id': args.id })
+            }
+        },
+        contributor: {
+            type: ContributorType,
+            args: { id: { type: GraphQLID } },
+            resolve (parent, args) {
+                return_.find(contributors, { 'id': args.id })
             }
         }
     }
