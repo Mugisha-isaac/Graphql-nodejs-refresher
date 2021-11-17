310a687e8d9ea4deeb84b1b9fa64d2c9a6436e87
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-11-17 08:37:27 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const PORT = process.env.port || 5000
+const app = express();
+
+
+app.use('',())
+
+
+app.listen(PORT,()=>{
+    console.log(`app is running on port ${PORT}`);
+})
